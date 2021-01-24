9bbc72f8d82897791c4e88a1780c2b7726c0cf4b
ymclapp <ymclapp@yahoo.com>
2021-01-24 16:37:25 -0600

    more server.js work to add error handlers

--- server.js
+++ server.js
@@ -2,15 +2,26 @@
 
 require('dotenv').config();
 
-const { response } = require('express');
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors()); //<<--need to have the inner parenthesis
+
+
+
+
 app.get('/', (request, response) => {
   response.send('City Explorer Goes Here!');
 });
+
+app.get('/bad', (request, response) => {
+  throw new Error('oops');
+});
+
+
 app.get('/location', (request, response) => {
   response.send('This will be where the location goes');
 });
@@ -19,5 +30,24 @@ app.get('/weather', (request, response) => {
   response.send('Weather!');
 });
 
-app.listen(PORT, () => console.log('App is listening on ${PORT}'));
+//Has to be after stuff loads too
+app.use(notFoundHandler);
+
+//Has to be after stuff loads
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));  //<<--these are tics not single quotes
+
+function errorHandler(error, request, response, next) {
+  response.status(500).json({
+    error: true,
+    message: error.message,
+  });
+}
+
+function notFoundHandler(request, response) {
+  response.status(404).json({
+    notFound:  true,
+  });
+}
 
