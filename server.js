c179062e45476d4bf6cc2ee540cbba59e9cf770a
ymclapp <ymclapp@yahoo.com>
2021-01-24 15:55:16 -0600

    added a /weather and /location in the server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 
 require('dotenv').config();
 
+const { response } = require('express');
 const express = require('express');
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('City Explorer Goes Here!');
+  response.send('City Explorer Goes Here!');
+});
+app.get('/location', (request, response) => {
+  response.send('This will be where the location goes');
+});
+
+app.get('/weather', (request, response) => {
+  response.send('Weather!');
 });
 
 app.listen(PORT, () => console.log('App is listening on ${PORT}'));
