14ecfb1c6c79dca53469fab6744cd132a220493f
ymclapp <ymclapp@yahoo.com>
2021-01-24 15:34:39 -0600

    trying Heroku auto deploy again after updating link to Git repo

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Home Page!');
+    response.send('City Explorer Goes Here!');
 });
 
 app.listen(PORT, () => console.log('App is listening on ${PORT}'));
