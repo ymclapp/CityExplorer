3728a58150c12264346e2b5af6d480604f493974
ymclapp <ymclapp@yahoo.com>
2021-01-29 16:55:31 -0600

    beautified code and found some more braces/brackets/parentheses that needed attention

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
-const { json } = require('express');
+const {
+  json
+} = require('express');
 
 const PORT = process.env.PORT;
 const app = express();
@@ -47,15 +49,15 @@ app.get('/weather', weatherHandler);
 // }
 
 function locationHandler(request, response) {
-  if(!process.env.GEOCODE_API_KEY) throw 'GEO_KEY not found';
+  if (!process.env.GEOCODE_API_KEY) throw 'GEO_KEY not found';
 
   const city = request.query.city;
   const url = 'https://us1.locationiq.com/v1/search.php';
   superagent.get(url)
     .query({
-      key:  process.env.GEOCODE_API_KEY,
-      q:  city,
-      format:  'json'
+      key: process.env.GEOCODE_API_KEY,
+      q: city,
+      format: 'json'
     })
     .then(locationResponse => {
       let geoData = locationResponse.body;
@@ -95,76 +97,76 @@ function weatherHandler(request, response) {
           console.log(err);
           errorHandler(err, request, response);
         })
-      }
-    
+    });
+}
 
-      // const weather = new Weather(weatherData);
-      // response.send(weatherResults);
-      // }
+// const weather = new Weather(weatherData);
+// response.send(weatherResults);
+// }
 
-      // function weatherHandler(request, response) {  //<<--this appears to work manually.  Need to get to work in City Exploerer and need to get the forEach to work.
-      //   const city = request.query.city;
-      //   const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
-      //   superagent.get(url)
-      //     .query({
-      //       city:  city,
-      //       key: process.env.WEATHER_API_KEY,
-      //       days: 4
-      //     })
-      //     .then(weatherResponse => {
-      //       let weatherData = weatherResponse.body.data;  //this is what comes back from API in json
-      //       console.log(weatherData);
+// function weatherHandler(request, response) {  //<<--this appears to work manually.  Need to get to work in City Exploerer and need to get the forEach to work.
+//   const city = request.query.city;
+//   const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
+//   superagent.get(url)
+//     .query({
+//       city:  city,
+//       key: process.env.WEATHER_API_KEY,
+//       days: 4
+//     })
+//     .then(weatherResponse => {
+//       let weatherData = weatherResponse.body.data;  //this is what comes back from API in json
+//       console.log(weatherData);
 
-      //       const weather = new Weather(city, weatherData);
-      //       response.send(weather);
-      //     })
-      //     .catch(err => {
-      //       console.log(err);
-      //       errorHandler(err, request, response);
-      //     });
-      // }
-
-
-
-      //Has to be after stuff loads too
-      app.use(notFoundHandler);
-
-      //Has to be after stuff loads
-      app.use(errorHandler);
-
-      app.listen(PORT, () => console.log(`App is listening on ${PORT}`)); //<<--these are tics not single quotes
+//       const weather = new Weather(city, weatherData);
+//       response.send(weather);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       errorHandler(err, request, response);
+//     });
+// }
 
 
-      //Route handlers
-      function errorHandler(error, request, response, next) {
-        console.error(error);
-        response.status(500).json({
-          error: true,
-          message: error.message,
-        });
-      }
 
-      function notFoundHandler(request, response) {
-        response.status(404).json({
-          notFound:  true,
-        });
-      }
+//Has to be after stuff loads too
+app.use(notFoundHandler);
+
+//Has to be after stuff loads
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`)); //<<--these are tics not single quotes
 
 
-      //Constructor functions
-      function Location (city, geoData) { //<<--this is saying that it needs city and geoData to be able to run the constructor
-        this.search_query = city;
-        this.formatted_query = geoData[0].display_name;
-        this.latitude = parseFloat(geoData[0].lat); //<<--used parseFloat because the info was a string and this will change to numbers
-        this.longitude = parseFloat(geoData[0].lon);
-      }
+//Route handlers
+function errorHandler(error, request, response, next) {
+  console.error(error);
+  response.status(500).json({
+    error: true,
+    message: error.message,
+  });
+}
 
-      // function Weather (weatherData) {  <<--does not work because the darksky file doesn't work for some reason
-      //   this.forecast = weatherData.summary;
-      //   this.time = weatherData.time;
-      // }
+function notFoundHandler(request, response) {
+  response.status(404).json({
+    notFound: true,
+  });
+}
 
-      function Weather (dailyWeather) {
-        this.forecast = dailyWeather.summary;
-        this.time = dailyWeather.datetime;
-      }
+
+//Constructor functions
+function Location(city, geoData) { //<<--this is saying that it needs city and geoData to be able to run the constructor
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = parseFloat(geoData[0].lat); //<<--used parseFloat because the info was a string and this will change to numbers
+  this.longitude = parseFloat(geoData[0].lon);
+}
+
+// function Weather (weatherData) {  <<--does not work because the darksky file doesn't work for some reason
+//   this.forecast = weatherData.summary;
+//   this.time = weatherData.time;
+// }
+
+function Weather(dailyWeather) {
+  this.forecast = dailyWeather.summary;
+  this.time = dailyWeather.datetime;
+}
