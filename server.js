19825d4b8fa82499ded218af5c64d6c5f088e05c
ymclapp <ymclapp@yahoo.com>
2021-01-29 16:36:20 -0600

    fixed some semicolons and braces/brackets/parentheses

--- server.js
+++ server.js
@@ -70,88 +70,101 @@ function locationHandler(request, response) {
     });
 }
 
-function weatherHandler(request, response) {  //<<--junk for when he is using darksky.json - this works, but darksky doesn't
- const latitude = request.query.latitude;
- const longitude = request.query.longitude;
+function weatherHandler(request, response) {
+  const latitude = request.query.latitude;
+  const longitude = request.query.longitude;
   const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
   superagent.get(url)
     .query({
       longitude: longitude,
-       latitude: latitude,
+      latitude: latitude,
       key: process.env.WEATHER_API_KEY,
       days: 4
     })
-  const weatherResults = [];  //<<--for returning an array of information
-  weatherData.daily.data.forEach(dailyWeather => {
-    weatherResults.push(new Weather(dailyWeather));
-  });
-  // const weather = new Weather(weatherData);
-  response.send(weatherResults);
-}
 
-// function weatherHandler(request, response) {  //<<--this appears to work manually.  Need to get to work in City Exploerer and need to get the forEach to work.
-//   const city = request.query.city;
-//   const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
-//   superagent.get(url)
-//     .query({
-//       city:  city,
-//       key: process.env.WEATHER_API_KEY,
-//       days: 4
-//     })
-//     .then(weatherResponse => {
-//       let weatherData = weatherResponse.body.data;  //this is what comes back from API in json
-//       console.log(weatherData);
+    .then(weatherResponse => {
+      let weatherData = weatherResponse.body.data; //this is what comes back from API in json
+      console.log(weatherData);
 
-//       const weather = new Weather(city, weatherData);
-//       response.send(weather);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       errorHandler(err, request, response);
-//     });
-// }
+      const weatherResults = []; //<<--for returning an array of information
+      weatherData.daily.data.forEach(dailyWeather => {
+        weatherResults.push(new Weather(dailyWeather));
+      })
 
+        .catch(err => {
+          console.log(err);
+          errorHandler(err, request, response);
+        })
+      }
+    
 
+      // const weather = new Weather(weatherData);
+      // response.send(weatherResults);
+      // }
 
-//Has to be after stuff loads too
-app.use(notFoundHandler);
+      // function weatherHandler(request, response) {  //<<--this appears to work manually.  Need to get to work in City Exploerer and need to get the forEach to work.
+      //   const city = request.query.city;
+      //   const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
+      //   superagent.get(url)
+      //     .query({
+      //       city:  city,
+      //       key: process.env.WEATHER_API_KEY,
+      //       days: 4
+      //     })
+      //     .then(weatherResponse => {
+      //       let weatherData = weatherResponse.body.data;  //this is what comes back from API in json
+      //       console.log(weatherData);
 
-//Has to be after stuff loads
-app.use(errorHandler);
-
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`)); //<<--these are tics not single quotes
-
-
-//Route handlers
-function errorHandler(error, request, response, next) {
-  console.error(error);
-  response.status(500).json({
-    error: true,
-    message: error.message,
-  });
-}
-
-function notFoundHandler(request, response) {
-  response.status(404).json({
-    notFound:  true,
-  });
-}
+      //       const weather = new Weather(city, weatherData);
+      //       response.send(weather);
+      //     })
+      //     .catch(err => {
+      //       console.log(err);
+      //       errorHandler(err, request, response);
+      //     });
+      // }
 
 
-//Constructor functions
-function Location (city, geoData) {  //<<--this is saying that it needs city and geoData to be able to run the constructor
-  this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = parseFloat(geoData[0].lat);  //<<--used parseFloat because the info was a string and this will change to numbers
-  this.longitude = parseFloat(geoData[0].lon);
-}
 
-// function Weather (weatherData) {  <<--does not work because the darksky file doesn't work for some reason
-//   this.forecast = weatherData.summary;
-//   this.time = weatherData.time;
-// }
+      //Has to be after stuff loads too
+      app.use(notFoundHandler);
 
-function Weather (weatherResults) {
-  this.forecast = weatherResults.summary;
-  this.time = weatherResults.datetime;
-}
+      //Has to be after stuff loads
+      app.use(errorHandler);
+
+      app.listen(PORT, () => console.log(`App is listening on ${PORT}`)); //<<--these are tics not single quotes
+
+
+      //Route handlers
+      function errorHandler(error, request, response, next) {
+        console.error(error);
+        response.status(500).json({
+          error: true,
+          message: error.message,
+        });
+      }
+
+      function notFoundHandler(request, response) {
+        response.status(404).json({
+          notFound:  true,
+        });
+      }
+
+
+      //Constructor functions
+      function Location (city, geoData) { //<<--this is saying that it needs city and geoData to be able to run the constructor
+        this.search_query = city;
+        this.formatted_query = geoData[0].display_name;
+        this.latitude = parseFloat(geoData[0].lat); //<<--used parseFloat because the info was a string and this will change to numbers
+        this.longitude = parseFloat(geoData[0].lon);
+      }
+
+      // function Weather (weatherData) {  <<--does not work because the darksky file doesn't work for some reason
+      //   this.forecast = weatherData.summary;
+      //   this.time = weatherData.time;
+      // }
+
+      function Weather (dailyWeather) {
+        this.forecast = dailyWeather.summary;
+        this.time = dailyWeather.datetime;
+      }
